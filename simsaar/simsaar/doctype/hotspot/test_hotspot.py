b1835ad460317e9905cf0cb322cbf42d580cafc7
Hudhaifa Al-sharabi <alsharabihudhaif@gmail.com>
2024-12-26 05:28:31 +0530

    add palce and hotspot doctype

--- simsaar/simsaar/doctype/hotspot/test_hotspot.py
+++ simsaar/simsaar/doctype/hotspot/test_hotspot.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestPlace(FrappeTestCase):
+class TestHotspot(FrappeTestCase):
 	pass
---
+++ simsaar/simsaar/doctype/property_type/property_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, hudhaifa alsharabi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Property Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
