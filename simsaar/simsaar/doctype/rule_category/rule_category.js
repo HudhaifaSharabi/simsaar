0f06304fffbe962a41d0b6b98f45d31f53a69263
Hudhaifa Al-sharabi <alsharabihudhaif@gmail.com>
2025-02-04 04:20:32 +0530

    add Stats Filde and remove childe table surroundings

--- simsaar/simsaar/doctype/rule_category/rule_category.js
+++ simsaar/simsaar/doctype/rule_category/rule_category.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, hudhaifa alsharabi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Surroundings", {
+// frappe.ui.form.on("Rule Category", {
 // 	refresh(frm) {
 
 // 	},
